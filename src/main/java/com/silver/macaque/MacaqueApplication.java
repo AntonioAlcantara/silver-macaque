d1b2371c11ce7328fe238efef8a7a2e4d49e75b9
Antonio <tonialcantara1997@gmail.com>
2021-04-10 18:53:52 +0200

    feat(initialize-project): Add a Spring Boot project for fun

---
+++ src/main/java/com/silver/macaque/MacaqueApplication.java
@@ -0,0 +1,13 @@
+package com.silver.macaque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacaqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacaqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silver/macaque/MacaqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silver.macaque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacaqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
